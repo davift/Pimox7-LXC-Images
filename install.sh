da30187b249f8d75cba35520c49e3579a96bb5eb
davift <73488542+davift@users.noreply.github.com>
2024-09-02 21:51:41 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo ''
+echo ''
+echo ''
+
+cd /var/lib/pve-manager/apl-info
+sudo mv releases.turnkeylinux.org /root 
+sudo mv download.proxmox.com /root
+
+
+
+echo ''
+echo 'Completed'
+echo ''
+exit 0
